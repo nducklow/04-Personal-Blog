733d27effdb2c831dbb5e2c2ca53f0c407be4ab8
Neil <nsducklow@gmail.com>
2024-04-25 15:55:57 -0500

    added landing page and basic submit and back button functionality

---
+++ assets/js/form.js
@@ -0,0 +1,9 @@
+const username= document.querySelector('#username')
+const title = document.querySelector('#title')
+const content = document.querySelector('#content')
+
+const submit = document.querySelector('#submit')
+
+submit.addEventListener('click', function(){
+    window.location.href = "./blog.html"
+ })
