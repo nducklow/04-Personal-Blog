b71c14bfa3e62c92bac4f8093fc1faf8b8cbffa2
Neil <nsducklow@gmail.com>
2024-05-02 09:31:18 -0500

    blog page properly displays current and past posts

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,17 +1,43 @@
-const username = document.querySelector('#username')
-const title = document.querySelector('#title')
-const content = document.querySelector('#content')
-const back = document.querySelector('#back')
+const usernameEl = document.querySelector('#username')
+const titleEl = document.querySelector('#title')
+const contentEl = document.querySelector('#content')
+const backEl = document.querySelector('#back')
+const postsEl = document.querySelector('#posts')
 
-back.addEventListener('click', function(){
-    window.location.href = "./index.html"
-})
+ let posts = []
 
 function init() {
-    // Get stored todos from localStorage
-   username.textContent = (localStorage.getItem('username'))
-   title.textContent = (localStorage.getItem('title')) 
-   content.textContent = (localStorage.getItem('content')) 
+    const currentPost = JSON.parse(localStorage.getItem('posts'))
+    const savedPosts = JSON.parse(localStorage.getItem('savedPosts'))
+    if (savedPosts !== null) {
+        posts = savedPosts;
+      }
+    console.log(currentPost)
+    posts.push(currentPost)
+    console.log(posts)
+    localStorage.setItem('savedPosts', JSON.stringify(posts))
+    
+    for (const post of posts) {
+        console.log(post.username)
+        const title = document.createElement('h2')
+        title.textContent = post.title
+        postsEl.appendChild(title)
+        const content = document.createElement('div')
+        content.textContent = post.content
+        postsEl.appendChild(content)
+        const username = document.createElement('h4')
+        username.textContent = post.username
+        postsEl.appendChild(username)
+
+    }
+    
+    backEl.addEventListener('click', function(){
+        window.location.href = "./index.html"
+    })
+    
 }
+
+
+
 
 init()
--- assets/js/form.js
+++ assets/js/form.js
@@ -3,25 +3,23 @@ const titleInput = document.querySelector('#title')
 const contentInput = document.querySelector('#content')
 const submit = document.querySelector('#submit')
 
-const blogPosts = []
 
 
 
 submit.addEventListener('click', function(event){
-    event.preventDefault()
+event.preventDefault()
 
-    const username = usernameInput.value.trim()
-    const title = titleInput.value.trim()
-    const content = contentInput.value.trim()
+const postInfo = {
+    username: usernameInput.value.trim(),
+    title: titleInput.value.trim(),
+    content: contentInput.value.trim(),
+}
 
-    localStorage.setItem('username', username)
-    localStorage.setItem('title', title)
-    localStorage.setItem('content', content)
-    
-    window.location.replace("./blog.html")
-    
-    
-    
+localStorage.setItem('posts', JSON.stringify(postInfo))
+
+window.location.replace("./blog.html")
+
+
  })
 
 
