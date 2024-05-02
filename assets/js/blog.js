4f53de51f43192c9ce674b36fd647c0179a49192
Neil <nsducklow@gmail.com>
2024-05-02 16:35:43 -0500

    added css to blog page

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -4,36 +4,37 @@ const contentEl = document.querySelector('#content')
 const backEl = document.querySelector('#back')
 const postsEl = document.querySelector('#posts')
 
- let posts = []
+let posts = []
 
 function init() {
-    backEl.addEventListener('click', function(){
+    backEl.addEventListener('click', function () {
         window.location.href = "./index.html"
     })
-    
+
     const currentPost = JSON.parse(localStorage.getItem('currentPost'))
     const savedPosts = JSON.parse(localStorage.getItem('savedPosts'))
     if (savedPosts !== null) {
         posts = savedPosts;
-      }
-    if (currentPost === null){
-        return
-
     }
-    posts.push(currentPost)
+    if (currentPost !== null) {
+        posts.push(currentPost)
+    }
     localStorage.setItem('savedPosts', JSON.stringify(posts))
-    
+
     for (const post of posts) {
-        console.log(post)
         const postArea = document.createElement('section')
+        postArea.setAttribute('class', 'container')
         const title = document.createElement('h2')
+        title.setAttribute('class', 'title')
         title.textContent = post.title
         postArea.appendChild(title)
         const content = document.createElement('div')
+        content.setAttribute('class', 'content')
         content.textContent = post.content
         postArea.appendChild(content)
-        const username = document.createElement('h4')
-        username.textContent = post.username
+        const username = document.createElement('footer')
+        username.setAttribute('class', 'username')
+        username.textContent = (`Posted by: ${post.username}`)
         postArea.appendChild(username)
         postsEl.appendChild(postArea)
 
