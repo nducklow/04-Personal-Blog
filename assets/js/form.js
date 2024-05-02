27f710b3c210afcaec0c36be22815414fe7fd533
Neil <nsducklow@gmail.com>
2024-05-02 17:30:56 -0500

    added no go prompt!

--- assets/js/form.js
+++ assets/js/form.js
@@ -6,27 +6,24 @@ const submit = document.querySelector('#submit')
 
 
 
-submit.addEventListener('click', function(event){
-event.preventDefault()
+submit.addEventListener('click', function (event) {
+    event.preventDefault()
 
-const postInfo = {
-    username: usernameInput.value.trim(),
-    title: titleInput.value.trim(),
-    content: contentInput.value.trim(),
-}
+    const postInfo = {
+        username: usernameInput.value.trim(),
+        title: titleInput.value.trim(),
+        content: contentInput.value.trim(),
+    }
+    if (postInfo.username || postInfo.title || postInfo.content !== '') {
+        localStorage.setItem('currentPost', JSON.stringify(postInfo))
 
-localStorage.setItem('currentPost', JSON.stringify(postInfo))
-
-window.location.replace("./blog.html")
-
-
-
-
- })
+    window.location.replace("./blog.html")
+    }
+    window.alert("One or more blanks are empty!")
+})
 
 
 
 
- 
 
- 
+
